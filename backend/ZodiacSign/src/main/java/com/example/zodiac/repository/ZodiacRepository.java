15225553ccfcaa9991e4c71149a809a7586a249d
eunseo <eunseobaeg178@gmail.com>
2025-06-23 17:05:16 +0900

    commit2

--- backend/ZodiacSign/src/main/java/com/example/zodiac/repository/ZodiacRepository.java
+++ backend/ZodiacSign/src/main/java/com/example/zodiac/repository/ZodiacRepository.java
@@ -1,13 +1,10 @@
 package com.example.zodiac.repository;
 
-import java.time.LocalDate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.zodiac.entity.Zodiac;
 
 public interface ZodiacRepository extends JpaRepository<Zodiac, Integer>{
-
-	Zodiac save(LocalDate birthDate);
 
 }
